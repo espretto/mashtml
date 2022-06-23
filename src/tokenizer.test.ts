d9323b7f7151c4e3c25ef0b3311854d5ea1e4498
espretto <espretto@posteo.net>
2022-06-24 00:10:02 +0200

    visibly skip instead of silently drop tests

--- src/tokenizer.test.ts
+++ src/tokenizer.test.ts
@@ -1,16 +1,4 @@
 import { tokenize } from "./tokenizer";
-import contentModelFlags from "../html5lib-tests/tokenizer/contentModelFlags.test";
-import domjs from "../html5lib-tests/tokenizer/domjs.test";
-import entities from "../html5lib-tests/tokenizer/entities.test";
-import escapeFlag from "../html5lib-tests/tokenizer/escapeFlag.test";
-import numericEntities from "../html5lib-tests/tokenizer/numericEntities.test";
-import pendingSpecChanges from "../html5lib-tests/tokenizer/pendingSpecChanges.test";
-import test1 from "../html5lib-tests/tokenizer/test1.test";
-import test2 from "../html5lib-tests/tokenizer/test2.test";
-import test3 from "../html5lib-tests/tokenizer/test3.test";
-import test4 from "../html5lib-tests/tokenizer/test4.test";
-import unicodeChars from "../html5lib-tests/tokenizer/unicodeChars.test";
-import unicodeCharsProblematic from "../html5lib-tests/tokenizer/unicodeCharsProblematic.test";
 import { Token, TokenType } from "./token";
 import { reNamedCharacterReference } from "./namedCharRefs";
 import { noop } from "./util";
@@ -38,6 +26,18 @@ function toTestToken(token: Token): TestToken {
       return ["Comment", token.data];
   }
 }
+
+/**
+ * used to preset tokenizer's initial state
+ */
+const lastStartTagMap: Record<InitialState, string> = {
+  "Data state": "",
+  "PLAINTEXT state": "<plaintext>",
+  "RCDATA state": "<title>",
+  "RAWTEXT state": "<style>",
+  "Script data state": "<script>",
+  "CDATA section state": "<![CDATA[",
+};
 
 /**
  * used to collect all tokens emitted
@@ -69,9 +69,14 @@ function coalesceAdjacentCharacterTokens(tokens: TestToken[]) {
   tokens.length = Math.min(tokens.length, newIndex + 1);
 }
 
-/**
- * used to unescape double-escaped test cases
- */
+function involvesEscapedScriptData({ input, initialStates }: TestCase) {
+  return input.includes("<!--") && initialStates?.includes("Script data state");
+}
+
+function involvesNamedCharacterReferences({ input }: TestCase) {
+  return reNamedCharacterReference.test(input);
+}
+
 function unescape(input: string) {
   return input.replace(/\\u([\da-f]{4})/gi, (_, hex) =>
     String.fromCodePoint(parseInt(hex, 16))
@@ -79,86 +84,86 @@ function unescape(input: string) {
 }
 
 /**
- * used to preset tokenizer's initial state
+ * used to unescape double-escaped test cases
  */
-const initialStateMap: Record<InitialState, [string, boolean]> = {
-  "Data state": ["", false],
-  "PLAINTEXT state": ["<plaintext>", true],
-  "RCDATA state": ["<title>", true],
-  "RAWTEXT state": ["<style>", true],
-  "Script data state": ["<script>", true],
-  "CDATA section state": ["<![CDATA[", false],
-};
+function unescapeTest(test: TestCase): TestCase {
+  if (!test.doubleEscaped) return test;
 
-const suites: Record<string, typeof test1.tests> = {
-  "html5lib-tests/tokenizer/contentModelFlags.test": contentModelFlags.tests,
-  "html5lib-tests/tokenizer/domjs.test": domjs.tests,
-  "html5lib-tests/tokenizer/entities.test": entities.tests,
-  "html5lib-tests/tokenizer/escapeFlag.test": escapeFlag.tests,
-  "html5lib-tests/tokenizer/numericEntities.test": numericEntities.tests,
-  "html5lib-tests/tokenizer/pendingSpecChanges.test": pendingSpecChanges.tests,
-  "html5lib-tests/tokenizer/test1.test": test1.tests,
-  "html5lib-tests/tokenizer/test2.test": test2.tests,
-  "html5lib-tests/tokenizer/test3.test": test3.tests,
-  "html5lib-tests/tokenizer/test4.test": test4.tests,
-  "html5lib-tests/tokenizer/unicodeChars.test": unicodeChars.tests,
-  "html5lib-tests/tokenizer/unicodeCharsProblematic.test":
-    unicodeCharsProblematic.tests,
-  // "html5lib-tests/tokenizer/namedEntities.test": namedEntities.tests, // not tested
-  // "html5lib-tests/tokenizer/xmlViolations.test": namedEntities.xmlViolationTests, // not tested
-};
+  return {
+    ...test,
+    doubleEscaped: false,
+    input: unescape(test.input),
+    output: test.output.map((token) =>
+      token[0] === "Character" ? ["Character", unescape(token[1])] : token
+    ),
+  };
+}
 
-Object.entries(suites).forEach(([suite, tests]) => {
-  describe(suite, () => tests.forEach(testCase));
-});
+const suites = [
+  "../html5lib-tests/tokenizer/contentModelFlags.test",
+  "../html5lib-tests/tokenizer/domjs.test",
+  "../html5lib-tests/tokenizer/entities.test",
+  "../html5lib-tests/tokenizer/escapeFlag.test",
+  "../html5lib-tests/tokenizer/numericEntities.test",
+  "../html5lib-tests/tokenizer/pendingSpecChanges.test",
+  "../html5lib-tests/tokenizer/test1.test",
+  "../html5lib-tests/tokenizer/test2.test",
+  "../html5lib-tests/tokenizer/test3.test",
+  "../html5lib-tests/tokenizer/test4.test",
+  "../html5lib-tests/tokenizer/unicodeChars.test",
+  "../html5lib-tests/tokenizer/unicodeCharsProblematic.test",
+  // "../html5lib-tests/tokenizer/namedEntities.test",
+  // "../html5lib-tests/tokenizer/xmlViolations.test",
+];
 
-function testCase({
-  description,
-  doubleEscaped,
-  input,
-  output: expected,
-  initialStates = ["Data state"],
-  lastStartTag,
-}: TestCase) {
-  if (doubleEscaped) {
-    input = unescape(input);
-    expected
-      .filter(([type]) => type === "Character")
-      .forEach((token) => (token[1] = unescape(token[1])));
+suites.forEach((suite) =>
+  describe(suite, () =>
+    (require(suite).tests as TestCase[]).map(unescapeTest).forEach(runTest)
+  )
+);
+
+function runTest(test: TestCase) {
+  if (
+    involvesEscapedScriptData(test) ||
+    involvesNamedCharacterReferences(test)
+  ) {
+    return it.skip(test.description, noop);
   }
 
-  if (reNamedCharacterReference.test(input)) {
-    return it.skip(description, noop);
-  }
+  const { initialStates = ["Data state"] } = test;
+  initialStates.forEach((initialState) => runTestInState(test, initialState));
+}
 
-  initialStates.forEach((initialState) => {
-    if (initialState === "Script data state") {
-      if (input.includes("</script>")) {
-        return it.skip(description, noop);
+function runTestInState(test: TestCase, initialState: InitialState) {
+  const { description, input, output: expected, lastStartTag } = test;
+
+  // set initial tokenizer state by prepending the corresponding tag
+  const trigger = lastStartTag
+    ? `<${lastStartTag}>`
+    : lastStartTagMap[initialState];
+
+  it(description, () => {
+    const actual = collectTokens(input ? trigger + input : input);
+
+    // remove prepended tag triggering the initial tokenizer state
+    if (trigger) {
+      const triggerToken = actual.shift();
+
+      // re-emit CDATA content as character tokens
+      if (triggerToken && initialState === "CDATA section state") {
+        actual.unshift(["Character", triggerToken[1]]);
       }
     }
 
-    const [stateTrigger, isTag] = lastStartTag
-      ? [`<${lastStartTag}>`, true]
-      : initialStateMap[initialState];
+    coalesceAdjacentCharacterTokens(actual);
 
-    it(description, () => {
-      const actual = collectTokens(stateTrigger + input);
-
-      if (isTag || !input) {
-        actual.shift();
-      } else if (initialState === "CDATA section state") {
-        actual[0][0] = "Character";
-      }
-
-      coalesceAdjacentCharacterTokens(actual);
-
-      if (expected[0]?.[0] === "DOCTYPE" || expected[0]?.[0] === "Comment") {
-        expect(actual).toHaveLength(expected.length);
-        expect(actual[0][0]).toEqual(expected[0][0]);
-      } else {
-        expect(actual).toEqual(expected);
-      }
-    });
+    // loosen test criteria for doctype and comment rules
+    const expectedType = expected[0]?.[0];
+    if (expectedType === "DOCTYPE" || expectedType === "Comment") {
+      expect(actual).toHaveLength(expected.length);
+      expect(actual[0][0]).toEqual(expectedType);
+    } else {
+      expect(actual).toEqual(expected);
+    }
   });
 }
